e254cba19ff9966f274d1ae37d1461e9eada7010
xiaowei <110343356+xiaowei-wei@users.noreply.github.com>
2025-05-10 10:16:46 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 // 数据库配置文件
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'gsdgfdg');
-define('DB_USER', 'root');
-define('DB_PASS', '123456');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
